966d81330d8dfcbd4e89194487529f96808fed7a
Nhatttk <nhatngo1205q@gmail.com>
2024-08-23 22:53:03 +0700

    add first screen

--- App.js
+++ App.js
@@ -1,50 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FirstScreens from './src/screens/First_Screens';
-import Details from './src/screens/Details';
-import HomeScreen from './src/screens/Home';
+import StackNavigator from './src/navigation/navigation';
 
-
-const Stack = createNativeStackNavigator()
-const Tab = createBottomTabNavigator()
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="Details" component={Details} />
-    </Tab.Navigator>
-  );
-}
-
-function StackNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="FirstScreens"
-        component={FirstScreens}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Details"
-        component={Details}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
-}
 
 export default function App() {
   return (
---
+++ src/navigation/navigation.js
@@ -0,0 +1,49 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FirstScreens from './../screens/First_Screens';
+import HomeScreen from './../screens/Home';
+import { Details } from './../screens/Details';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="Details" component={Details} />
+    </Tab.Navigator>
+  );
+}
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="FirstScreens"
+        component={FirstScreens}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default StackNavigator;
+
+
